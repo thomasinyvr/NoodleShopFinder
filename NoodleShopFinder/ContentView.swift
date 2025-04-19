03f71ce9db3fdd70bb9bd681cfc217e17efa192b
thomasinyvr <tomfriesman@gmail.com>
2025-04-18 22:35:52 -0700

    Initial Commit

---
+++ NoodleShopFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoodleShopFinder
+//
+//  Created by Thomas Friesman on 2025-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoodleShopFinder/NoodleShopFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoodleShopFinderApp.swift
+//  NoodleShopFinder
+//
+//  Created by Thomas Friesman on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct NoodleShopFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
